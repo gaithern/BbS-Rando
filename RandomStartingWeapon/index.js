3c7d1cd657637ba000a68c985e26089296647acb
gaithern <gaithern@gmail.com>
2024-07-12 22:24:45 -0500

    Add starting weapon to stock

--- RandomStartingWeapon/index.js
+++ RandomStartingWeapon/index.js
@@ -1,12 +1,13 @@
 // Initializes variables for currently held weapon (All characters)
 const currentWeapon = "version_choice({0x0, 0x10F9DE65}, game_version)"
 const currentWeaponInMenu = "version_choice({0x0, 0x10FA51DE}, game_version)"
+const stockWeapon = "version_choice({0x0, 0x10FA2AAA}, game_version)"
 
 // Initializes function that randomizes the starting Weapon and removes it from the pool
 export function randomStartingWeapon(startingWeapon, keyblades, weaponCheck) {
   const randomWeapon = Math.floor(Math.random() * startingWeapon.length);
   const removedWeaponMenu = keyblades.splice(randomWeapon, 1)[0];
   const removedWeapon = startingWeapon.splice(randomWeapon, 1)[0];
-  const randomstartingWeapon = `${weaponCheck.value}\nWriteByte(${currentWeapon}, 0x${removedWeapon.digit})\nWriteShort(${currentWeaponInMenu}, 0x${removedWeaponMenu.digit})\nend`;
+  const randomstartingWeapon = `${weaponCheck.value}\nWriteByte(${currentWeapon}, 0x${removedWeapon.digit})\nWriteShort(${currentWeaponInMenu}, 0x${removedWeaponMenu.digit})\nWriteShort(${stockWeapon}, 0x${removedWeaponMenu.digit})\nend`;
   return randomstartingWeapon;
 }
